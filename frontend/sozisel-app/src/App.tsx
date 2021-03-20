70981d711708b3730b2c10a45a3dc02ad2d3e296
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-03-20 13:30:35 +0100

    Added react app basic setup

---
+++ frontend/sozisel-app/src/App.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react";
+
+export default function App(): ReactElement {
+  return <h1>Welcome to Sozisel App and once more!!</h1>;
+}
---
+++ frontend/sozisel-app/webpack.config.js
@@ -0,0 +1,113 @@
+/* eslint-disable */
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const DEV = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  // change me once in production
+  devtool: "source-map",
+  entry: {
+    app: "./src/index.tsx",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+              publicPath: "/bundle/fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jp(e)?g|png)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "/bundle/images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+        template: path.join(__dirname, 'src', 'index.html')
+    }),
+    new CleanWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        enabled: true,
+        files: "./src/**/*.{ts,tsx,js,jsx}",
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/", to: "../" }],
+    }),
+    !DEV
+      ? new CompressionPlugin({
+          filename: "[path][base].gz",
+          test: /\.(js|css|html|svg)$/,
+          algorithm: "gzip",
+        })
+      : false,
+    !DEV
+      ? new CompressionPlugin({
+          filename: "[path][base].br",
+          test: /\.(js|css|html|svg)$/,
+          algorithm: "brotliCompress",
+        })
+      : false,
+  ].filter(Boolean),
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    open: true,
+    overlay: true,
+  },
+};
