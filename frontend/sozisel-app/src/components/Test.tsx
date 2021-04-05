f287c592109285c9389e5e35ee8d9543cde73408
jmysliv <jjmreal34@gmail.com>
2021-04-05 19:13:47 +0200

    Added lint script

--- frontend/sozisel-app/src/components/Test.tsx
+++ frontend/sozisel-app/src/components/Test.tsx
@@ -5,26 +5,25 @@ import JitsiFrame from "./JitsiFrame";
 
 export default function Test(): ReactElement {
   const { data: queryData } = useGetHelloQuery();
-  const {data} = useHelloMessagesSubscription();
-  
+  const { data } = useHelloMessagesSubscription();
+
   const [messages, setMessages] = useState<string[]>([]);
-  
+
   useEffect(() => {
     if (queryData?.hello.message) {
-      setMessages(messages => [queryData.hello.message, ...messages]);
+      setMessages((messages) => [queryData.hello.message, ...messages]);
     }
   }, [queryData]);
-  
 
   useEffect(() => {
     if (data?.helloMessages?.message) {
       const message = data.helloMessages.message;
-      setMessages(messages => [message, ...messages]);
+      setMessages((messages) => [message, ...messages]);
     }
   }, [data]);
 
   return (
-    <div style={{display: "flex", flexDirection: "column"}}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <JitsiFrame />
     </div>
   );
