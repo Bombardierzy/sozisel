f87a8def940da733f1467e881939f21399f3bbb7
Jakub Myśliwiec <48885911+jmysliv@users.noreply.github.com>
2021-04-08 13:00:20 +0200

    [Task #8] CI pipeline (#14)
    * Added github action for backend
    
    * Added ci for frontend
    
    * Fixed working directory
    
    * Added lint script
    
    * Added build script
    
    * Added uploading artifact with graphql schema
    
    * Fixed workflow yaml
    
    * Fixed path for download artifact
    
    * Added format check
    
    * Changed script name

--- frontend/sozisel-app/src/components/JitsiFrame.tsx
+++ frontend/sozisel-app/src/components/JitsiFrame.tsx
@@ -2,9 +2,9 @@ import Jitsi from "react-jitsi";
 import { ReactElement } from "react";
 
 export default function JitsiFrame(): ReactElement {
-    return (
-        <>
-            <Jitsi domain="localhost:8443" roomName="aaa"></Jitsi>
-        </>
-    );
+  return (
+    <>
+      <Jitsi domain="localhost:8443" roomName="aaa"></Jitsi>
+    </>
+  );
 }
