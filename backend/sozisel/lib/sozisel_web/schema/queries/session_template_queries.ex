e44a4e1fdac25ac7510607cfa66223a4cec8eaec
Jakub Perżyło <qizot1405@gmail.com>
2021-05-08 13:20:27 +0200

    Added session template by id query (#62)

--- backend/sozisel/lib/sozisel_web/schema/queries/session_template_queries.ex
+++ backend/sozisel/lib/sozisel_web/schema/queries/session_template_queries.ex
@@ -4,6 +4,13 @@ defmodule SoziselWeb.Schema.Queries.SessionTemplateQueries do
   alias SoziselWeb.Schema.{Middleware, Resolvers.SessionTemplateResolvers}
 
   object :session_template_queries do
+    field :session_template, :session_template do
+      arg :id, non_null(:id)
+
+      middleware Middleware.Authorization
+      resolve &SessionTemplateResolvers.get_session_template/3
+    end
+
     field :search_session_templates, strong_list_of(:session_template) do
       arg :include_public, :boolean, default_value: false
       arg :name, :string, default_value: ""
