7a073551869d747faf7abcb65f7f10020a61c514
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-05-22 18:38:39 +0200

    Added placeholder translation

--- frontend/sozisel-app/src/components/LiveSession/LiveSession.tsx
+++ frontend/sozisel-app/src/components/LiveSession/LiveSession.tsx
@@ -3,6 +3,7 @@ import "./LiveSession.scss";
 import { ReactElement } from "react";
 import { useLiveSessionParticipation } from "../../hooks/useLiveSessionParticipation";
 import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 interface LiveSessionParams {
   sessionId: string;
@@ -10,15 +11,16 @@ interface LiveSessionParams {
 
 export function LiveSession(): ReactElement {
   const { sessionId } = useParams<LiveSessionParams>();
+  const {t} = useTranslation("common");
 
   const { participants, error, loading } = useLiveSessionParticipation({
     displayName: "User",
     sessionId,
   });
-
+  
   return (
     <div className="LiveSession">
-      <h2>You are inside live session!</h2>
+      <h2>{t("components.LiveSession.placeholder")}</h2>
       {participants.length > 0 && (
         <div className="participantsList">
           {participants.map(({ displayName, key }) => (
