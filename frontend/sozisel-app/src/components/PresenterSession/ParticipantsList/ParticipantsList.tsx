3b4bf09e44c5075bc9360625f2dd7aaef2875974
Sebastian Kuśnierz <47144579+skusnierz@users.noreply.github.com>
2021-06-07 16:27:37 +0200

    [Task #72] Added participants list view (#99)
    * Added participants list view
    
    * Added connection with backend
    
    * Minor fixes
    
    * Formated code
    
    * Added presenter icon
    
    * Formated code

---
+++ frontend/sozisel-app/src/components/PresenterSession/ParticipantsList/ParticipantsList.tsx
@@ -0,0 +1,65 @@
+import "./ParticipantsList.scss";
+
+import { Alert, Skeleton } from "@material-ui/lab";
+import { Paper, Typography } from "@material-ui/core";
+import React, { ReactElement } from "react";
+
+import GroupIcon from "@material-ui/icons/Group";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import PersonIcon from "@material-ui/icons/Person";
+import { useLiveSessionParticipation } from "../../../hooks/useLiveSessionParticipation";
+import useSessionParticipantType from "../../../hooks/useSessionParticipantType";
+import { useTranslation } from "react-i18next";
+
+interface ParticipantsListProps {
+  sessionId: string;
+}
+
+export default function ParticipantsList({
+  sessionId,
+}: ParticipantsListProps): ReactElement {
+  const { token, type } = useSessionParticipantType();
+  const { t } = useTranslation("common");
+  const { participants, error, loading } = useLiveSessionParticipation({
+    sessionId,
+    type,
+    token,
+  });
+
+  return (
+    <Paper elevation={2} className="ParticipantsList">
+      <Typography className="header">
+        <GroupIcon className="icon" />
+        {t("components.PresenterSession.ParticipantsList.header")}
+      </Typography>
+      {error && (
+        <Alert className="errorAlert" variant="outlined" severity="error">
+          {t("components.PresenterSession.ParticipantsList.fetchingError")}
+        </Alert>
+      )}
+      {loading && (
+        <>
+          <Skeleton height={30} />
+          <Skeleton height={30} />
+          <Skeleton height={30} />
+        </>
+      )}
+      {participants.length > 0 &&
+        participants.map(({ id, displayName, type }) => (
+          <div key={id} className="participant">
+            {type == "presenter" ? (
+              <>
+                <Typography className="presenter">{displayName}</Typography>
+                <PersonIcon className="presenter" />
+              </>
+            ) : (
+              <>
+                <Typography>{displayName}</Typography>
+                <MoreVertIcon className="moreIcon" />
+              </>
+            )}
+          </div>
+        ))}
+    </Paper>
+  );
+}
