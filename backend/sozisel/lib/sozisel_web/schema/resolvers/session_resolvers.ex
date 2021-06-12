88510402a1e05d5437969bca33d4ccc18321df0d
jmysliv <jjmreal34@gmail.com>
2021-06-12 11:40:07 +0200

    Formatted code

--- backend/sozisel/lib/sozisel_web/schema/resolvers/session_resolvers.ex
+++ backend/sozisel/lib/sozisel_web/schema/resolvers/session_resolvers.ex
@@ -26,7 +26,10 @@ defmodule SoziselWeb.Schema.Resolvers.SessionResolvers do
         session
         |> Map.put(:password_required, session.entry_password != nil)
         |> Map.put(:agenda_entries, Sessions.list_agenda_entries(session.session_template_id))
-        |> Map.put(:estimated_time, Sessions.get_template(session.session_template_id).estimated_time)
+        |> Map.put(
+          :estimated_time,
+          Sessions.get_template(session.session_template_id).estimated_time
+        )
 
       {:ok, thumbnail}
     else
