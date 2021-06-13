4bd3436ef485d5809f39da4076f995e98bc9dabc
Sebastian <sebastiano139@gmail.com>
2021-06-13 12:41:59 +0200

    Formated code

--- frontend/sozisel-app/src/components/SessionsList/SessionCard/SessionCard.tsx
+++ frontend/sozisel-app/src/components/SessionsList/SessionCard/SessionCard.tsx
@@ -68,7 +68,7 @@ export default function SessionCard({
   };
 
   const onCardClick = () => {
-    if(isEnded) {
+    if (isEnded) {
       history.push({
         pathname: `/sessions/${session.id}/result`,
       });
