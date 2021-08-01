93885c5f6d3a5a37c8ef30bed082a9646ea3627c
jmysliv <jjmreal34@gmail.com>
2021-08-01 19:56:57 +0200

    Adjusted to review

--- frontend/sozisel-app/src/components/utils/Avatar/CustomAvatar.tsx
+++ frontend/sozisel-app/src/components/utils/Avatar/CustomAvatar.tsx
@@ -3,8 +3,14 @@ import useAvatarById from "../../../hooks/useAvatarById";
 
 export interface CustomAvatarProps {
   id: string;
+  width?: number;
 }
-export default function CustomAvatar({ id }: CustomAvatarProps): ReactElement {
+export default function CustomAvatar({
+  id,
+  width = 150,
+}: CustomAvatarProps): ReactElement {
   const avatar = useAvatarById(id);
-  return <img width="151" src={`data:image/svg+xml;base64,${btoa(avatar)}`} />;
+  return (
+    <img width={width} src={`data:image/svg+xml;base64,${btoa(avatar)}`} />
+  );
 }
