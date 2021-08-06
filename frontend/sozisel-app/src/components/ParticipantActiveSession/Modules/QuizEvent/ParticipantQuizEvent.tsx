304a9afbf8639a0f84e96040e35bcf3a7c733ac4
jmysliv <jjmreal34@gmail.com>
2021-08-06 17:08:48 +0200

    Removed quiz answer time from mutation

--- frontend/sozisel-app/src/components/ParticipantActiveSession/Modules/QuizEvent/ParticipantQuizEvent.tsx
+++ frontend/sozisel-app/src/components/ParticipantActiveSession/Modules/QuizEvent/ParticipantQuizEvent.tsx
@@ -49,9 +49,6 @@ export default function ParticipantQuizEvent({
         input: {
           launchedEventId: event.id,
           participantAnswers: finalAnswers,
-          quizAnswerTime: finalAnswers
-            .map((element) => element.answerTime)
-            .reduce((prev, curr) => prev + curr),
         },
       },
     });
