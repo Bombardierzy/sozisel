737dc17637875177dfbd29a43c905631cbb2a0e6
jmysliv <jjmreal34@gmail.com>
2021-08-25 20:56:51 +0200

    Added generic table

--- frontend/sozisel-app/src/components/SessionResultScreen/SessionResultEvents/EventResultDetails/Modules/QuizResultDetails/QuizResultViews/Participants/QuizResultParticipantsView.tsx
+++ frontend/sozisel-app/src/components/SessionResultScreen/SessionResultEvents/EventResultDetails/Modules/QuizResultDetails/QuizResultViews/Participants/QuizResultParticipantsView.tsx
@@ -1,5 +1,36 @@
+import "./QuizResultParticipantsView.scss";
+
+import EnhancedTable from "../../../../../../../utils/Table/Table";
+import PeopleIcon from "@material-ui/icons/People";
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 export default function QuizResultParticipantsView(): React.ReactElement {
-  return <>Participants</>;
+  const participants: { name: string; age: number }[] = [
+    { name: "test", age: 20 },
+    { name: "seba", age: 22 },
+    { name: "seba", age: 22 },
+    { name: "seba", age: 22 },
+    { name: "seba", age: 22 },
+    { name: "seba", age: 22 },
+  ];
+
+  return (
+    <div className="QuizResultParticipantsView">
+      <div className="headerWithIcon">
+        <PeopleIcon color="primary" fontSize="large" />
+        <Typography variant="h3" className="header">
+          Uczestnicy
+        </Typography>
+      </div>
+      <EnhancedTable
+        data={participants}
+        headCells={[
+          { id: "name", label: "name" },
+          { id: "age", label: "age" },
+        ]}
+        onClick={(a) => console.log(a)}
+      />
+    </div>
+  );
 }
