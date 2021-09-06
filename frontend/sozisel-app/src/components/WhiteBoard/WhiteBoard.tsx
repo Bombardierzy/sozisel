5d708f2ad8caf1e0d9f5e1fd8bf061e3874f9091
Jakub Perżyło <qizot1405@gmail.com>
2021-09-06 22:06:09 +0200

    [Task #139] Added whiteboard component with shared board capabilities (#137)
    * Added basic whiteboard setup without shared board yet
    
    * Added shared board module in backend
    
    * Integrated white board with shared board socket
    
    * Self-review
    
    * Updated error message for shared board channel
    
    * Formatted code

---
+++ frontend/sozisel-app/src/components/WhiteBoard/WhiteBoard.tsx
@@ -0,0 +1,14 @@
+import { Canvas } from "./Canvas";
+import { CanvasToolbar } from "./CanvasToolbar";
+import Navbar from "../Navbar/LoginNavbar/Navbar";
+import { ReactElement } from "react";
+
+export function WhiteBoard(): ReactElement {
+  return (
+    <>
+      <Navbar />
+      <CanvasToolbar />
+      <Canvas width={1200} height={900} sessionId="lobby" />
+    </>
+  );
+}
