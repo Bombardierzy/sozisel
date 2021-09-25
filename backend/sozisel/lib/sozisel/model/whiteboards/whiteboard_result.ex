c542fb218b23efb915d7b71de0de4aba27c4224a
Przemysław Wątroba <przemyslaw.watroba@gmail.com>
2021-09-25 23:53:08 +0200

    Add whiteboard summary

--- backend/sozisel/lib/sozisel/model/whiteboards/whiteboard_result.ex
+++ backend/sozisel/lib/sozisel/model/whiteboards/whiteboard_result.ex
@@ -1,7 +1,11 @@
 defmodule Sozisel.Model.Whiteboards.WhiteboardResult do
   use Sozisel.Model.Schema
-
   import Ecto.Changeset
+
+  alias Sozisel.Repo
+  alias Sozisel.Model.Events.Event
+  alias Sozisel.Model.Whiteboards.Whiteboard
+  alias Sozisel.Model.LaunchedEvents.LaunchedEvent
 
   @type t :: %__MODULE__{
           path: String.t(),
@@ -27,5 +31,43 @@ defmodule Sozisel.Model.Whiteboards.WhiteboardResult do
   @spec generate_filename(Ecto.UUID.t(), Ecto.UUID.t(), String.t()) :: String.t()
   def generate_filename(launched_event_id, participant_id, extension) do
     "whiteboard_#{launched_event_id}_#{participant_id}#{extension}" |> String.replace(" ", "_")
+  end
+
+  @spec whiteboard_summary(Ecto.UUID.t()) :: map() | nil
+  def whiteboard_summary(launched_event_id) do
+    with %LaunchedEvent{} = launched_event <- Repo.get(LaunchedEvent, launched_event_id),
+         %LaunchedEvent{
+           event: %Event{
+             event_data: %Whiteboard{
+               task: task
+             }
+           },
+           event_results: event_results
+         } <- Repo.preload(launched_event, [:event, :event_results, event_results: :participant]) do
+
+      answers =
+        event_results
+        |> Enum.map(fn participant_result ->
+          %{
+            full_name: participant_result.participant.full_name,
+            email: participant_result.participant.email,
+            image_path: participant_result.result_data.path,
+            used_time: participant_result.result_data.used_time,
+            additional_text: participant_result.result_data.text
+          }
+        end)
+    
+      total_used_time =
+        event_results
+        |> Enum.map(& &1.result_data.used_time)
+        |> Enum.sum()
+
+      %{
+        id: launched_event_id,
+        task: task,
+        average_used_time: (total_used_time / length(event_results)) |> Float.round(2),
+        participants_tasks: answers
+      }
+    end
   end
 end
--- backend/sozisel/lib/sozisel_web/schema/resolvers/launched_event_resolvers.ex
+++ backend/sozisel/lib/sozisel_web/schema/resolvers/launched_event_resolvers.ex
@@ -1,5 +1,5 @@
 defmodule SoziselWeb.Schema.Resolvers.LaunchedEventResolvers do
-  alias Sozisel.Model.{LaunchedEvents, Quizzes.QuizResult, Polls.PollResult}
+  alias Sozisel.Model.{LaunchedEvents, Quizzes.QuizResult, Polls.PollResult, Whiteboards.WhiteboardResult}
   alias LaunchedEvents.LaunchedEvent
 
   import SoziselWeb.Schema.Middleware.ResourceAuthorization, only: [fetch_resource!: 2]
@@ -24,5 +24,9 @@ defmodule SoziselWeb.Schema.Resolvers.LaunchedEventResolvers do
 
   def poll_summary(_parent, %{id: launched_event_id}, _ctx) do
     {:ok, PollResult.poll_summary(launched_event_id)}
+  end
+
+  def whiteboard_summary(_parent, %{id: launched_event_id}, _ctx) do
+    {:ok, WhiteboardResult.whiteboard_summary(launched_event_id)}
   end
 end
