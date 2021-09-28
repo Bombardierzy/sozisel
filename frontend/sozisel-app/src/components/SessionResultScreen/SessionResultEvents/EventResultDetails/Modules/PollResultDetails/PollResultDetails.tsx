7978306a07231bb461c4408e919e2e08a65aedaf
jmysliv <jjmreal34@gmail.com>
2021-09-28 19:24:37 +0200

    Added piechart

--- frontend/sozisel-app/src/components/SessionResultScreen/SessionResultEvents/EventResultDetails/Modules/PollResultDetails/PollResultDetails.tsx
+++ frontend/sozisel-app/src/components/SessionResultScreen/SessionResultEvents/EventResultDetails/Modules/PollResultDetails/PollResultDetails.tsx
@@ -1,11 +1,13 @@
 import "./PollResultDetails.scss";
 
-import { CircularProgress, Paper, Typography } from "@material-ui/core";
+import { CircularProgress, List, Paper, Typography } from "@material-ui/core";
 
 import ErrorAlert from "../../../../../utils/Alerts/ErrorAlert";
 import EventIcon from "@material-ui/icons/Event";
+import PollPieChart from "../../../../../utils/PollPieChart/PollPieChart";
 import React from "react";
 import StatsRow from "../../../../../utils/StatsRow/StatsRow";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import { usePollResultQuery } from "../../../../../../graphql";
 import { useTranslation } from "react-i18next";
 
@@ -44,26 +46,47 @@ export default function PollResultDetails({
           </Typography>
           {
             <StatsRow
-              label={"Liczba uczestników"}
+              label={t(
+                "components.SessionEventResults.Poll.participantsNumber"
+              )}
               value={`${data.pollSummary.totalVoters}`}
             />
           }
           {
             <StatsRow
-              label={"Wielokrotny wybór"}
+              label={t("components.SessionEventResults.Poll.multichoice")}
               value={`${data.pollSummary.isMultiChoice ? "Tak" : "Nie"}`}
             />
           }
-          <Typography className="answersLabel">Liczba odpowiedzi</Typography>
-          {data.pollSummary.optionSummaries.map((option) => (
-            <StatsRow
-              key={option.id}
-              label={option.text}
-              value={`${option.votes}`}
-            />
-          ))}
+          <Typography className="answersLabel">
+            {t("components.SessionEventResults.Poll.answersNumber")}
+          </Typography>
+          <List className="answersList">
+            {data.pollSummary.optionSummaries.map((option) => (
+              <StatsRow
+                key={option.id}
+                label={option.text}
+                value={`${option.votes}`}
+              />
+            ))}
+          </List>
         </Paper>
-        <Paper className="pollResultView" elevation={2}></Paper>
+        <Paper className="pollResultView" elevation={2}>
+          <div className="pollResultHeader">
+            <TrendingUpIcon color="primary" fontSize="large" />
+            <Typography variant="h3" className="resultHeader">
+              {t("components.SessionEventResults.Quiz.charts")}
+            </Typography>
+          </div>
+          <div className="pollPieChart">
+            <PollPieChart
+              outerRadius={150}
+              data={data.pollSummary.optionSummaries.filter(
+                (option) => option.votes > 0
+              )}
+            />
+          </div>
+        </Paper>
       </div>
     );
   }
