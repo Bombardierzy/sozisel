8fa0dbff8f17184e83ea95994c190a2fc8cd4a9c
Jakub Perżyło <qizot1405@gmail.com>
2021-10-14 17:57:22 +0200

    Fixed configs for deployment

--- backend/sozisel/config/prod.exs
+++ backend/sozisel/config/prod.exs
@@ -1,11 +1,5 @@
-use Mix.Config
+import Config
 
-config :sozisel, SoziselWeb.Endpoint,
-  # TODO host must be the server_ip injected in dokcer-compose and .env file
-  url: [host: "example.pl", port: 4000],
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
-# Do not print debug messages in production
 config :logger, level: :info
 
-import_config "prod.secret.exs"
+config :phoenix, :server, true
---
+++ backend/sozisel/config/runtime.exs
@@ -0,0 +1,18 @@
+use Config
+
+if config_env() == :prod do
+  config :sozisel, SoziselWeb.Endpoint,
+    # TODO host must be the server_ip injected in dokcer-compose and .env file
+    url: [host: System.fetch_env!("HOST"), port: System.fetch_env!("PORT")],
+    secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+
+  # DATABASE_URL
+  # For example: ecto://USER:PASS@HOST/DATABASE
+  config :sozisel, Sozisel.Repo,
+    username: System.fetch_env!("POSTGRES_USER"),
+    password: System.fetch_env!("POSTGRES_PASS"),
+    database: System.fetch_env!("POSTGRES_DB"),
+    hostname: System.fetch_env!("POSTGRES_HOST"),
+    port: System.fetch_env!("POSTGRES_PORT"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+end
