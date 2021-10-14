36d8fea8eab12b55fa79d7604b684416444a33bf
Przemysław Wątroba <przemyslaw.watroba@gmail.com>
2021-10-14 15:08:02 +0200

    Add files necessarry to deploy

--- backend/sozisel/config/prod.exs
+++ backend/sozisel/config/prod.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :sozisel, SoziselWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  # TODO host must be the server_ip injected in dokcer-compose and .env file
+  url: [host: "example.pl", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
