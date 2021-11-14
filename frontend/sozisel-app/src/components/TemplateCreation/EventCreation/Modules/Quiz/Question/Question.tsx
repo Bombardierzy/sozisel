8441e901bde828d3c20f28612d715a7b77ff7b6f
skusnier <sebastiano139@gmail.com>
2021-11-14 12:29:40 +0100

    Add checbox for tick correct answer for quiz question

--- frontend/sozisel-app/src/components/TemplateCreation/EventCreation/Modules/Quiz/Question/Question.tsx
+++ frontend/sozisel-app/src/components/TemplateCreation/EventCreation/Modules/Quiz/Question/Question.tsx
@@ -1,6 +1,11 @@
 import "./Question.scss";
 
-import { InputAdornment, TextField } from "@material-ui/core";
+import {
+  Checkbox,
+  InputAdornment,
+  TextField,
+  Tooltip,
+} from "@material-ui/core";
 import React, { ReactElement } from "react";
 
 import AddCircleIcon from "@material-ui/icons/AddCircle";
@@ -9,8 +14,6 @@ import { QuizQuestion } from "../../../../../../model/Template";
 import { useQuizContext } from "../../../../../../contexts/Quiz/QuizContext";
 import { useTranslation } from "react-i18next";
 import { v4 as uuidv4 } from "uuid";
-
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
 interface NewQuestionProp {
   question: QuizQuestion;
@@ -23,11 +26,17 @@ export default function Question({ question }: NewQuestionProp): ReactElement {
   return (
     <div className="Question">
       <div className="buttonRight">
-        <ClearIcon
-          color="primary"
-          className="check"
-          onClick={() => dispatch({ type: "DELETE_QUESTION", question })}
-        />
+        <Tooltip
+          title={t<string>(
+            "components.TemplateCreation.Quiz.Question.deleteQuestion"
+          )}
+        >
+          <ClearIcon
+            color="primary"
+            className="check"
+            onClick={() => dispatch({ type: "DELETE_QUESTION", question })}
+          />
+        </Tooltip>
       </div>
       <TextField
         size="small"
@@ -45,69 +54,102 @@ export default function Question({ question }: NewQuestionProp): ReactElement {
         }
       />
 
-      {question.answers.map((answer, idx) => (
-        <TextField
-          key={idx}
-          size="small"
-          placeholder={t(
-            "components.TemplateCreation.Quiz.Question.defaultAnswer"
-          )}
-          value={answer.text}
-          onChange={(e) =>
-            dispatch({
-              type: "UPDATE_ANSWER",
-              answer: { ...answer, text: e.target.value },
-              question,
-            })
-          }
-          variant="outlined"
-          onClick={() =>
-            dispatch({
-              type: "TOGGLE_CORRECT_ANSWER",
-              question,
-              correctAnswer: answer,
-            })
-          }
-          className={`answer ${
-            question.correctAnswers.filter(
-              (correctAnswer) => correctAnswer.id === answer.id
-            ).length > 0 && "correctAnswer"
-          }`}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">{alphabet[idx]}</InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment position="start">
-                <ClearIcon
-                  color="primary"
-                  className="check"
-                  onClick={() =>
-                    dispatch({ type: "DELETE_ANSWER", question, answer })
-                  }
-                />
-              </InputAdornment>
-            ),
-          }}
-        />
-      ))}
+      {question.answers.map((answer, idx) => {
+        const isChecked =
+          question.correctAnswers.filter(
+            (correctAnswer) => correctAnswer.id === answer.id
+          ).length > 0;
+
+        return (
+          <TextField
+            key={idx}
+            size="small"
+            placeholder={t(
+              "components.TemplateCreation.Quiz.Question.defaultAnswer"
+            )}
+            value={answer.text}
+            onChange={(e) =>
+              dispatch({
+                type: "UPDATE_ANSWER",
+                answer: { ...answer, text: e.target.value },
+                question,
+              })
+            }
+            variant="outlined"
+            className={`answer ${isChecked && "correctAnswer"}`}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Tooltip
+                    title={
+                      isChecked
+                        ? t<string>(
+                            "components.TemplateCreation.Quiz.Question.uncheckCorrectAnswer"
+                          )
+                        : t<string>(
+                            "components.TemplateCreation.Quiz.Question.checkCorrectAnswer"
+                          )
+                    }
+                  >
+                    <Checkbox
+                      color="primary"
+                      className="check"
+                      onClick={() =>
+                        dispatch({
+                          type: "TOGGLE_CORRECT_ANSWER",
+                          question,
+                          correctAnswer: answer,
+                        })
+                      }
+                      checked={isChecked}
+                    />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="start">
+                  <Tooltip
+                    title={t<string>(
+                      "components.TemplateCreation.Quiz.Question.deleteAnswer"
+                    )}
+                  >
+                    <ClearIcon
+                      color="primary"
+                      className="check"
+                      onClick={() =>
+                        dispatch({ type: "DELETE_ANSWER", question, answer })
+                      }
+                    />
+                  </Tooltip>
+                </InputAdornment>
+              ),
+            }}
+          />
+        );
+      })}
 
       <div className="buttonFlex">
-        <AddCircleIcon
-          fontSize="large"
-          className="addButton"
-          color="primary"
-          onClick={() => {
-            dispatch({
-              type: "ADD_ANSWER",
-              question,
-              answer: {
-                id: uuidv4(),
-                text: "",
-              },
-            });
-          }}
-        />
+        <Tooltip
+          title={t<string>(
+            "components.TemplateCreation.Quiz.Question.addAnswer"
+          )}
+        >
+          <AddCircleIcon
+            fontSize="large"
+            className="addButton"
+            color="primary"
+            onClick={() => {
+              dispatch({
+                type: "ADD_ANSWER",
+                question,
+                answer: {
+                  id: uuidv4(),
+                  text: "",
+                },
+              });
+            }}
+          />
+        </Tooltip>
       </div>
     </div>
   );
