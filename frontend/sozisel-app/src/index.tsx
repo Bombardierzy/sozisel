41fe43016a67240e54cbf8c7f64c84e4514c1034
Jakub Perżyło <qizot1405@gmail.com>
2022-04-08 21:04:21 +0200

    Fixed linter

--- frontend/sozisel-app/src/index.tsx
+++ frontend/sozisel-app/src/index.tsx
@@ -1,14 +1,14 @@
+import "./index.scss";
 import { ThemeProvider } from "@material-ui/core";
 import { createMuiTheme } from "@material-ui/core/styles";
-import i18next from "i18next";
-import ReactDOM from "react-dom";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import {
   default as commonEN,
   default as commonPL,
 } from "../public/locales/pl/common.json";
+import i18next from "i18next";
+import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.scss";
 
 i18next.use(initReactI18next).init({
   interpolation: { escapeValue: false },
